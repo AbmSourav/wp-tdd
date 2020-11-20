b6d5525290500dfa1984eb7c94b6e2c453c7e7eb
AbmSourav <keramotul.islam@gmail.com>
2020-11-20 11:49:40 +0600

    test

---
+++ plugin-name.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: WP TDD
+ * Description: A Test-Driven Development WordPress plugin starter.
+ * Version:     0.0.2
+ * Author:      Keramot UL Islam
+ * Author URI:  https://abmsourav.com/
+ * Requires PHP: 5.4
+ * License: GPLv2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wp-tdd
+ *
+ * @package wpTDD
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+echo esc_html__( 'Hello Universe.....', 'wp-tdd' );
